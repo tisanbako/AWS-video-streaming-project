db372b64ca8144e83a2b088075f00634a47c3ccb
tisanbako <bakotisan@gmail.com>
2024-03-08 12:12:57 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to Tisan Bako Workout Streaming Services</h2>
+      <video width="700px" height="400" controls>
+        <source src="https://disq2kklgrjqy.cloudfront.net/tisan-workout.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
